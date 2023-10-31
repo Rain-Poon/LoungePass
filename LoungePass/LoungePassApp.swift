b63bf1bfe843364035702c916cd362406b6d32ba
Rain-Poon <85403806+Rain-Poon@users.noreply.github.com>
2023-10-31 15:00:53 +0800

    First commit

---
+++ LoungePass/LoungePassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoungePassApp.swift
+//  LoungePass
+//
+//  Created by Rain Poon on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoungePassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
